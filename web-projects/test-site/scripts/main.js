b263fe1ceeaa2186418b48ef8b6e2951d2c47664
LoneJune <1261345068@qq.com>
2024-09-15 22:21:09 +0800

    Update main.js

--- web-projects/test-site/scripts/main.js
+++ web-projects/test-site/scripts/main.js
@@ -11,19 +11,19 @@ myImage.onclick = function () {
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 function setUserName() {
-  let myName = prompt("请输入你的名字。");
+  let myName = prompt("请输入你的名字");
   if (!myName) {
     setUserName();
   } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = myName + "很cool" ;
+    myHeading.textContent = myName + "很cool！" ;
   }
 }
 if (!localStorage.getItem("name")) {
   setUserName();
 } else {
   let storedName = localStorage.getItem("name");
-  myHeading.textContent = storedName + "很cool" ;
+  myHeading.textContent = storedName + "很cool！" ;
 }
 myButton.onclick = function () {
   setUserName();
